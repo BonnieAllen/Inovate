259a2810c5ee1bc68a0bdeffd570b1173227a04b
Bonnie <aalle.allen@gmail.com>
2015-09-07 17:55:10 -0400

    Added profile to index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def avatar_url(staffer)
+		gravatar_id = Digest::MD5.hexdigest(staffer.email.downcase)
+		"http://gravatar.com/avatar/#{gravatar_id}.png?s=80%d"
+	end
 end
